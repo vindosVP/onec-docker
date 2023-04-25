cf51e26cae73dae9eb6f42971e4508a68f962115
ivanovEV <ivanovev@melonfashion.com>
2023-04-25 18:08:12 +0300

    Удалил deprecated

--- (deprecated) client-vnc-jenkins.bat
+++
@@ -1,5 +0,0 @@
-docker build ^
-  --build-arg ONEC_VERSION=%ONEC_VERSION% ^
-  --build-arg DOCKER_USERNAME=%DOCKER_USERNAME% ^
-  -t %DOCKER_USERNAME%/onec-client-vnc-jenkins:%ONEC_VERSION% ^
-  -f client-vnc-jenkins/Dockerfile .
--- (deprecated) oscript.bat
+++
@@ -1,6 +0,0 @@
-docker build ^
-  --build-arg DOCKER_USERNAME=%DOCKER_USERNAME% ^
-  --build-arg BASE_IMAGE=onec-client-vnc ^
-  --build-arg BASE_TAG=%ONEC_VERSION% ^
-  -t %DOCKER_USERNAME%/client-oscript:%ONEC_VERSION% ^
-  -f oscript/Dockerfile .
